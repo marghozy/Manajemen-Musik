93999f94e5517656daca6e1e9a82e2923628d4cf
marghozy <162287890+marghozy@users.noreply.github.com>
2024-10-13 20:17:57 +0700

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,17 @@
+public class Song {
+    private String nama;
+    private String artis;
+
+    public Song(String nama, String artis) {
+        this.nama = nama;
+        this.artis = artis;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getArtis() {
+        return artis;
+    }
+}
